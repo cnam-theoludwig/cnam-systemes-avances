126e34ab445d364b283319ef2516e46ca8271014
Théo LUDWIG <contact@theoludwig.fr>
2025-10-22 16:28:40 +0200

    feat: add 01-syscalls-fs-3-ls

--- 01-syscalls-fs-3-ls/src/main.c
+++ 01-syscalls-fs-3-ls/src/main.c
@@ -1,43 +1,72 @@
-#include "./cli_utils.h"
+#include <dirent.h>
+#include <errno.h>
+#include <grp.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
 
-int main(int argc, char** argv) {
-  struct cli_params cli_params;
-  cli_main(argc, &argv, &cli_params);
+#define MAX_PATH_LENGTH 4096
+#define MAX_DATE_BUFFER_LENGTH 64
 
-  int file_input = open(cli_params.input_path, O_RDONLY);
-  int file_output = open(cli_params.output_path, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+void print_permissions(mode_t file_mode) {
+  char permissions[11];
 
-  if (file_input == -1) {
-    perror("Error open(file_input)");
+  permissions[0] = S_ISDIR(file_mode) ? 'd' : '-';
+  permissions[1] = (file_mode & S_IRUSR) ? 'r' : '-';
+  permissions[2] = (file_mode & S_IWUSR) ? 'w' : '-';
+  permissions[3] = (file_mode & S_IXUSR) ? 'x' : '-';
+  permissions[4] = (file_mode & S_IRGRP) ? 'r' : '-';
+  permissions[5] = (file_mode & S_IWGRP) ? 'w' : '-';
+  permissions[6] = (file_mode & S_IXGRP) ? 'x' : '-';
+  permissions[7] = (file_mode & S_IROTH) ? 'r' : '-';
+  permissions[8] = (file_mode & S_IWOTH) ? 'w' : '-';
+  permissions[9] = (file_mode & S_IXOTH) ? 'x' : '-';
+  permissions[10] = '\0';
 
-    free(cli_params.input_path);
-    free(cli_params.output_path);
+  printf("%s", permissions);
+}
+
+int main(int argc, char const** argv) {
+  const char* directory_path = (argc > 1) ? argv[1] : "./";
+
+  DIR* directory_pointer = opendir(directory_path);
+  if (directory_pointer == NULL) {
+    perror("Error opendir(directory_path)");
     return EXIT_FAILURE;
   }
-  if (file_output == -1) {
-    perror("Error open(file_output)");
 
-    close(file_input);
-    free(cli_params.input_path);
-    free(cli_params.output_path);
-    return EXIT_FAILURE;
-  }
+  struct dirent* directory_entry;
+  struct stat file_stat;
+  char full_file_path[MAX_PATH_LENGTH];
 
-  size_t bytes_read = 0;
-  size_t bytes_write = 0;
-  char buffer[BUFFER_SIZE];
-
-  while ((bytes_read = read(file_input, buffer, BUFFER_SIZE)) > 0) {
-    bytes_write = write(file_output, &buffer, bytes_read);
-    if (bytes_write != bytes_read) {
-      break;
+  while ((directory_entry = readdir(directory_pointer)) != NULL) {
+    if (strcmp(directory_entry->d_name, ".") == 0 || strcmp(directory_entry->d_name, "..") == 0) {
+      continue;
     }
+
+    snprintf(full_file_path, sizeof(full_file_path), "%s/%s", directory_path, directory_entry->d_name);
+
+    if (stat(full_file_path, &file_stat) == -1) {
+      perror("Error stat()");
+      continue;
+    }
+
+    struct passwd* password_entry = getpwuid(file_stat.st_uid);
+    struct group* group_entry = getgrgid(file_stat.st_gid);
+
+    char date_buffer[MAX_DATE_BUFFER_LENGTH];
+    struct tm* time_information = localtime(&file_stat.st_mtime);
+    strftime(date_buffer, sizeof(date_buffer), "%d%m%y @ %Hh%M", time_information);
+
+    printf("%-20s - ", directory_entry->d_name);
+    print_permissions(file_stat.st_mode);
+    printf(" %s : %s - %ld - %s\n", (password_entry != NULL ? password_entry->pw_name : "unknown"), (group_entry != NULL ? group_entry->gr_name : "unknown"), file_stat.st_size, date_buffer);
   }
 
-  close(file_input);
-  free(cli_params.input_path);
-
-  close(file_output);
-  free(cli_params.output_path);
+  closedir(directory_pointer);
   return EXIT_SUCCESS;
 }
