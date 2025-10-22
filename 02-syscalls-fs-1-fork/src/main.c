1894cf53fcc8e4488ae90a7df22d5d7aa4fea51b
Djelal BOUDJI <djelal2004@gmail.com>
2025-10-22 16:30:14 +0200

    feat: print pid for fork yourself exercice

---
+++ 02-syscalls-fs-1-fork/src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid;
+    int status;
+
+    pid = fork();
+    if (pid < 0) {
+        perror("Fork failed");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        fprintf(stdout, "Child process : my PID is (PID: %d), Parent PID is : %d\n", getpid(), getppid());
+        int exit_code = getpid() % 10;
+        fprintf(stdout, "Child process : Exiting with code %d\n", exit_code);
+        exit(exit_code); 
+    } else {
+        fprintf(stdout, "Parent process (PID: %d), created Child PID: %d\n", getpid(), pid);
+        wait(&status);
+
+        if (WIFEXITED(status)) {
+            int exit_status = WEXITSTATUS(status);
+            fprintf(stdout, "Parent process (PID: %d), Child PID: %d exited with status: %d\n", getpid(), pid, exit_status);
+        } else {
+            fprintf(stdout, "Child process did not terminate normally.\n");
+        }
+    }
+
+    return 0;
+}
