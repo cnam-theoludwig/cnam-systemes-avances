319b03fd22bcf728521390f236ef927ad51349be
Théo LUDWIG <contact@theoludwig.fr>
2025-11-19 11:19:59 +0100

    fix: feedbacks review

--- 02-syscalls-process-3-redirect-pipe/src/main.c
+++ 02-syscalls-process-3-redirect-pipe/src/main.c
@@ -15,40 +15,40 @@ int main(void) {
 
   if (pipe(pipe_file_descriptors) == -1) {
     perror("pipe");
-    return errno;
+    return EXIT_FAILURE;
   }
 
   process_id_ps = fork();
   if (process_id_ps == -1) {
     perror("fork");
-    return errno;
+    return EXIT_FAILURE;
   }
 
   if (process_id_ps == 0) {
     if (close(pipe_file_descriptors[0]) == -1) {
       perror("close");
-      exit(errno);
+      exit(EXIT_FAILURE);
     }
 
     if (dup2(pipe_file_descriptors[1], STDOUT_FILENO) == -1) {
       perror("dup2");
-      exit(errno);
+      exit(EXIT_FAILURE);
     }
 
     if (close(pipe_file_descriptors[1]) == -1) {
       perror("close");
-      exit(errno);
+      exit(EXIT_FAILURE);
     }
 
     execlp("ps", "ps", "eaux", NULL);
     perror("execlp");
-    exit(errno);
+    exit(EXIT_FAILURE);
   }
 
   process_id_grep = fork();
   if (process_id_grep == -1) {
     perror("fork");
-    return errno;
+    return EXIT_FAILURE;
   }
 
   if (process_id_grep == 0) {
@@ -56,38 +56,38 @@ int main(void) {
 
     if (close(pipe_file_descriptors[1]) == -1) {
       perror("close");
-      exit(errno);
+      exit(EXIT_FAILURE);
     }
 
     if (dup2(pipe_file_descriptors[0], STDIN_FILENO) == -1) {
       perror("dup2");
-      exit(errno);
+      exit(EXIT_FAILURE);
     }
 
     if (close(pipe_file_descriptors[0]) == -1) {
       perror("close");
-      exit(errno);
+      exit(EXIT_FAILURE);
     }
 
     null_file_descriptor = open("/dev/null", O_WRONLY);
     if (null_file_descriptor == -1) {
       perror("open");
-      exit(errno);
+      exit(EXIT_FAILURE);
     }
 
     if (dup2(null_file_descriptor, STDOUT_FILENO) == -1) {
       perror("dup2");
-      exit(errno);
+      exit(EXIT_FAILURE);
     }
 
     if (close(null_file_descriptor) == -1) {
       perror("close");
-      exit(errno);
+      exit(EXIT_FAILURE);
     }
 
     execlp("grep", "grep", "^root ", NULL);
     perror("execlp");
-    exit(errno);
+    exit(EXIT_FAILURE);
   }
 
   if (close(pipe_file_descriptors[0]) == -1) {
@@ -97,13 +97,23 @@ int main(void) {
     perror("close");
   }
 
-  waitpid(process_id_ps, NULL, 0);
-  waitpid(process_id_grep, &execution_status, 0);
+  if (waitpid(process_id_ps, NULL, 0) == -1) {
+    perror("waitpid");
+    return EXIT_FAILURE;
+  }
+
+  if (waitpid(process_id_grep, &execution_status, 0) == -1) {
+    perror("waitpid");
+    return EXIT_FAILURE;
+  }
 
   if (WIFEXITED(execution_status)) {
     if (WEXITSTATUS(execution_status) == 0) {
       const char* message = "root est connecté\n";
-      write(STDOUT_FILENO, message, strlen(message));
+      if (write(STDOUT_FILENO, message, strlen(message)) == -1) {
+        perror("write");
+        return EXIT_FAILURE;
+      }
     }
   }
 
